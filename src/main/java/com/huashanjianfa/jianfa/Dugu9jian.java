25db58f6d24f63d3984fabcbc477bdf658924b7a
codejiwei <1622264511@qq.com>
2020-12-14 20:04:20 +0800

    Initial commit

---
+++ src/main/java/com/huashanjianfa/jianfa/Dugu9jian.java
@@ -0,0 +1,19 @@
+package com.huashanjianfa.jianfa;
+
+/**
+ * @ClassName Dugu9jian
+ * @Description TODO
+ * @Author codejiwei
+ * @Date 2020/12/14 15:08
+ * @Version 1.0
+ **/
+public class Dugu9jian {
+    public static void main(String[] args) {
+
+        System.out.println("华山剑法");
+        System.out.println("华山剑法");
+        System.out.println("华山剑法");
+        System.out.println("华山剑法");
+
+    }
+}
