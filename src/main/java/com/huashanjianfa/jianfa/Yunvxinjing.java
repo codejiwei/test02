972b81b484752130857394a10adb2b8edf4fe3cd
codejiwei <1622264511@qq.com>
2020-12-14 20:17:38 +0800

    玉女心经 紫霞剑法

---
+++ src/main/java/com/huashanjianfa/jianfa/Yunvxinjing.java
@@ -0,0 +1,19 @@
+package com.huashanjianfa.jianfa;
+
+/**
+ * @ClassName Yunvxinjing
+ * @Description TODO
+ * @Author codejiwei
+ * @Date 2020/12/14 20:09
+ * @Version 1.0
+ **/
+public class Yunvxinjing {
+    public static void main(String[] args) {
+        System.out.println("玉女心经");
+        System.out.println("玉女心经");
+        System.out.println("玉女心经");
+        System.out.println("玉女心经");
+        System.out.println("玉女心经");
+
+    }
+}
---
+++ src/main/java/com/huashanjianfa/jianfa/Zixiajianfa.java
@@ -0,0 +1,20 @@
+package com.huashanjianfa.jianfa;
+
+/**
+ * @ClassName Zixiajianfa
+ * @Description TODO
+ * @Author codejiwei
+ * @Date 2020/12/14 20:12
+ * @Version 1.0
+ **/
+public class Zixiajianfa {
+    public static void main(String[] args) {
+
+        System.out.println("紫霞剑法");
+        System.out.println("紫霞剑法");
+        System.out.println("紫霞剑法");
+        System.out.println("紫霞剑法");
+        System.out.println("紫霞剑法");
+
+    }
+}
