52184c107cb929cfc9610131964ac7288d74da9e
codejiwei <1622264511@qq.com>
2020-12-15 08:53:39 +0800

    排山倒海

---
+++ src/main/java/com/huashanjianfa/jianfa/Paishandaohai.java
@@ -0,0 +1,20 @@
+package com.huashanjianfa.jianfa;
+
+/**
+ * @ClassName Paishandaohai
+ * @Description TODO
+ * @Author codejiwei
+ * @Date 2020/12/15 8:52
+ * @Version 1.0
+ **/
+public class Paishandaohai {
+    public static void main(String[] args) {
+
+        System.out.println("排山倒海");
+        System.out.println("排山倒海");
+        System.out.println("排山倒海");
+        System.out.println("排山倒海");
+        System.out.println("排山倒海");
+
+    }
+}
